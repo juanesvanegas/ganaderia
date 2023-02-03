12a3212b798f517202ca0b15031d5ec63ca697f7
Elkin1802 <elkinocampo277@gmail.com>
2023-02-03 14:37:13 -0500

    nuevas rutas

--- routes/web.php
+++ routes/web.php
@@ -29,72 +29,70 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 
 //categoria
-Route::get('/categoria',                    [CategoriaController::class,'index'])->name('index.categoria');
-Route::get('/categoria',                    [CategoriaController::class,'index1'])->name('menu.categoria');
-Route::get('/categoria/create',             [CategoriaController::class,'create'])->name('create.categoria');
-Route::post('/categoria/create/store',      [CategoriaController::class,'store'])->name('store.categoria');
-Route::get('/categoria/edit/{categoria}',   [CategoriaController::class,'edit'])->name('edit.categoria');
-Route::put('/categoria/{categoria}',        [CategoriaController::class,'update'])->name('update.categoria');
-Route::delete('/categoria/{categoria}',     [CategoriaController::class,'destroy'])->name('destroy.categoria');
+Route::get('/categoria',                    [CategoriaController::class,'index'])->middleware('auth')->name('index.categoria');
+Route::get('/categoria',                    [CategoriaController::class,'index1'])->middleware('auth')->name('menu.categoria');
+Route::get('/categoria/create',             [CategoriaController::class,'create'])->middleware('auth')->name('create.categoria');
+Route::post('/categoria/create/store',      [CategoriaController::class,'store'])->middleware('auth')->name('store.categoria');
+Route::get('/categoria/edit/{categoria}',   [CategoriaController::class,'edit'])->middleware('auth')->name('edit.categoria');
+Route::put('/categoria/{categoria}',        [CategoriaController::class,'update'])->middleware('auth')->name('update.categoria');
+Route::delete('/categoria/{categoria}',     [CategoriaController::class,'destroy'])->middleware('auth')->name('destroy.categoria');
 
 
 // ------------------------------------------------------------------------------------------------------------
 
 //Archivo
 
-Route::get('/archivo',                          [ArchivoController::class,'index'])->name('index.archivo');
-Route::get('/archivo/create',                   [ArchivoController::class,'create'])->name('create.archivo');
-Route::post('/archivo/create/store',            [ArchivoController::class,'store'])->name('store.archivo');
-Route::delete('/archivo/eliminar/{archivo}',    [ArchivoController::class,'destroy'])->name('destroy.archivo');
-Route::get('/archivo/{archivo}/edit',           [ArchivoController::class,'edit' ])->name('archivo.edit');
-Route::put('/archivo/{archivo}',                [ArchivoController::class,'update' ])->name('archivo.update');
+Route::get('/archivo',                          [ArchivoController::class,'index'])->middleware('auth')->name('index.archivo');
+Route::get('/archivo/create',                   [ArchivoController::class,'create'])->middleware('auth')->name('create.archivo');
+Route::post('/archivo/create/store',            [ArchivoController::class,'store'])->middleware('auth')->name('store.archivo');
+Route::delete('/archivo/eliminar/{archivo}',    [ArchivoController::class,'destroy'])->middleware('auth')->name('destroy.archivo');
+Route::get('/archivo/{archivo}/edit',           [ArchivoController::class,'edit' ])->middleware('auth')->name('archivo.edit');
+Route::put('/archivo/{archivo}',                [ArchivoController::class,'update' ])->middleware('auth')->name('archivo.update');
 
 // ----------------------------------------------------------------------------------------------------------------
 
 // Medicamento
 
-Route::get('/medicamento',                    [ MedicamentoController ::class,'index'])->name('index.medicamento');
-Route::get('/medicamento/create',             [MedicamentoController::class,'create'])->name('create.medicamento');
-Route::post('/medicamento/create/store',      [MedicamentoController::class,'store'])->name('store.medicamento');
-Route::delete('/medicamento/{medicamento}',   [MedicamentoController::class,'destroy'])->name('destroy.medicamento');
-Route::get('/medicamento/{medicamento}/edit', [MedicamentoController::class,'edit' ])->name('medicamento.edit');
-Route::put('/medicamento/{medicamento}',      [MedicamentoController::class,'update' ])->name('medicamento.update');
+Route::get('/medicamento',                    [ MedicamentoController ::class,'index'])->middleware('auth')->name('index.medicamento');
+Route::get('/medicamento/create',             [MedicamentoController::class,'create'])->middleware('auth')->name('create.medicamento');
+Route::post('/medicamento/create/store',      [MedicamentoController::class,'store'])->middleware('auth')->name('store.medicamento');
+Route::delete('/medicamento/{medicamento}',   [MedicamentoController::class,'destroy'])->middleware('auth')->name('destroy.medicamento');
+Route::get('/medicamento/{medicamento}/edit', [MedicamentoController::class,'edit' ])->middleware('auth')->name('medicamento.edit');
+Route::put('/medicamento/{medicamento}',      [MedicamentoController::class,'update' ])->middleware('auth')->name('medicamento.update');
 
 
 // ----------------------------------------------------------------------------------------------------------------
 
 // Animal
 
-Route::get('/animal',                   [ AnimalController ::class,'index'])->name('index.animal');
-Route::get('/animal/create',            [AnimalController::class,'create'])->name('create.animal');
-Route::post('/animal/create/store',     [AnimalController::class,'store'])->name('store.animal');
-Route::delete('/animal/{animal}',       [AnimalController::class,'destroy'])->name('destroy.animal');
-Route::get('/animal/{animal}/edit',     [AnimalController::class,'edit' ])->name('animal.edit');
-Route::put('/animal/{animal}',          [AnimalController::class,'update' ])->name('animal.update');
+Route::get('/animal',                   [ AnimalController ::class,'index'])->middleware('auth')->middleware('auth')->name('index.animal');
+Route::get('/animal/create',            [AnimalController::class,'create'])->middleware('auth')->name('create.animal');
+Route::post('/animal/create/store',     [AnimalController::class,'store'])->middleware('auth')->name('store.animal');
+Route::delete('/animal/{animal}',       [AnimalController::class,'destroy'])->middleware('auth')->name('destroy.animal');
+Route::get('/animal/{animal}/edit',     [AnimalController::class,'edit' ])->middleware('auth')->name('animal.edit');
+Route::put('/animal/{animal}',          [AnimalController::class,'update' ])->middleware('auth')->name('animal.update');
 
 
 // ----------------------------------------------------------------------------------------------------------------
 
 // Enfermedad
 
-Route::get('/enfermedad',                       [EnfermedadeController ::class,'index'])->name('index.enfermedad');
-Route::get('/enfermedad/create',                [EnfermedadeController::class,'create'])->name('create.enfermedad');
-Route::post('/enfermedad/create/store',         [EnfermedadeController::class,'store'])->name('store.enfermedad');
-Route::delete('/enfermedad/{enfermedade}',      [EnfermedadeController::class,'destroy'])->name('destroy.enfermedad');
-Route::get('/enfermedad/{enfermedade}/edit',    [EnfermedadeController::class,'edit' ])->name('enfermedad.edit');
-Route::put('/enfermedad/{enfermedade}',         [EnfermedadeController::class,'update' ])->name('enfermedad.update');
+Route::get('/enfermedad',                       [EnfermedadeController ::class,'index'])->middleware('auth')->middleware('auth')->name('index.enfermedad');
+Route::get('/enfermedad/create',                [EnfermedadeController::class,'create'])->middleware('auth')->name('create.enfermedad');
+Route::post('/enfermedad/create/store',         [EnfermedadeController::class,'store'])->middleware('auth')->name('store.enfermedad');
+Route::delete('/enfermedad/{enfermedade}',      [EnfermedadeController::class,'destroy'])->middleware('auth')->name('destroy.enfermedad');
+Route::get('/enfermedad/{enfermedade}/edit',    [EnfermedadeController::class,'edit' ])->middleware('auth')->name('enfermedad.edit');
+Route::put('/enfermedad/{enfermedade}',         [EnfermedadeController::class,'update' ])->middleware('auth')->name('enfermedad.update');
 
 // ----------------------------------------------------------------------------------------------------------------
 
 // Vacunacion
 
-Route::get('/vacunacion',                   [ VacunacioneController ::class,'index'])->name('index.vacunacion');
-Route::get('/vacunacion/create',            [VacunacioneController::class,'create'])->name('create.vacunacion');
-Route::post('/vacunacion/create/store',     [VacunacioneController::class,'store'])->name('store.vacunacion');
-Route::delete('/vacunacion/{vacunacione}',  [VacunacioneController::class,'destroy'])->name('destroy.vacunacion');
-Route::get('/vacunacion/{vacunacione}/edit',[VacunacioneController::class,'edit' ])->name('vacunacion.edit');
-Route::put('/vacunacion/{vacunacione}',     [VacunacioneController::class,'update' ])->name('vacunacion.update');
+Route::get('/vacunacion',                   [ VacunacioneController ::class,'index'])->middleware('auth')->middleware('auth')->name('index.vacunacion');
+Route::get('/vacunacion/create',            [VacunacioneController::class,'create'])->middleware('auth')->name('create.vacunacion');
+Route::post('/vacunacion/create/store',     [VacunacioneController::class,'store'])->middleware('auth')->name('store.vacunacion');
+Route::delete('/vacunacion/{vacunacione}',  [VacunacioneController::class,'destroy'])->middleware('auth')->name('destroy.vacunacion');
+Route::get('/vacunacion/{vacunacione}/edit',[VacunacioneController::class,'edit' ])->middleware('auth')->name('vacunacion.edit');
+Route::put('/vacunacion/{vacunacione}',     [VacunacioneController::class,'update' ])->middleware('auth')->name('vacunacion.update');
 
 // ----------------------------------------------------------------------------------------------------------------
-
-
